68d03cc3cda8aeb5f8c3cbca73c4c6725189271a
fyfuy110 <111558412+fyfuy110@users.noreply.github.com>
2022-08-25 21:07:02 +0500

    Update and rename README.md to AKING2.py

---
+++ AKING2.py
@@ -0,0 +1,53 @@
+^[[1;37m[+]══════════════════════════════════════>
+   ## ##   ##   ##   ##  ###   ## ##    ##
+  ##   ##  ##  ##    ##  ####  ## ##
+ ##     ## #####     ##  ## ## ## ##   ####
+ ######### ##  ##    ##  ##  #### ##    ##
+ ##     ## ##   ##   ##  ##   ### ##    ##
+ ##     ## ##    ## #### ##    ##  ###### PK❤
+[+]══════════════════════════════════════════
+[+] Author    : IMTIAZ AKING
+[+] Github    : AKING110
+[+] Facebook  : MR.AKING.07
+[+] Tool Type : Free Trail
+[+] ^[[1;32mEnjoy free trail and share with frien>
+^[[1;37m[+]══════════════════════════════════════>
+[2] Random Cloning (BD)
+[3] Random Gamil Cloning
+[4] WhatsApp Group ^[[1;33m{Join-Now}^[[1;37m
+[5] Facebook Group
+[0] ^[[1;91mExit Menu^@2^@/2022/06/^@/2022/07/ua.>
+[√] BD CODES: 013,014,015,012...^[[1;37m
+[!]══════════════════════════════════════════
+[•] PUT CODE: ^@BarColumn^@BeautifulSoup^@
+[×] Bs4 module not installed!...
+^@C^@CVmLKfRaHKBEizR4Ru6CK3^@[•] Cloning Complete
+[•] Total OK IDS : ^@ConnectionError^@[×] Connect>
+[•] LIMIT : ^@Expired Apk & Web Not Found ^@F^@
+[×] Futures module not installed!...
+^@G^@Gml^@H^@ImportError^@
+(!) Info : Found Some Error! ^@[√] JAZZ CODE: 030>
+[√] ZONG CODE: 0315,0312,0314...
+[√] TLNR CODE: 0345,0341,0344... ^[[1;37m
+[!]══════════════════════════════════════════
+[•] PUT CODE : ^@Kedaluwarsa^@ Kedaluwarsa^@L^@Lo>
+[•] CP IDS SAVE : /sdcard/AKING-CP.txt^@P^@Premiu>
+[+] Note : Try Your Sim Network Codes
+[+] Expp : My Network is Zong i try Zong Codes
+[+] IF IDs Not Come Then Power of Device For 5 Mi>
+[•] Total CP IDS : ^@Trail^@[•] User IP : ^@Value>
+^@^M^M^M^@ [^@] [^@]^@ - ^@^M^M^@[^@×^@] ^@a^@acc>
+[×] requests module not installed!...
+^@rich^@
+[×] rich module not installed!...
+^@rich.progress^@[√] %sActive Applications & Webs>
+return next yielded value or raise StopIteration.>
+return next yielded value or raise StopIteration.>
+    try: Generator = _module.Generator
+    except AttributeError: pass
+else: Generator.register(_cython_generator_ty>
+if _cython_coroutine_type is not None:
+    try: Coroutine = _module.Coroutine
+    except AttributeError: pass
+    else: Coroutine.register(_cython_coroutine_ty>
+^@need more than %zd value%.1s to unpack^@__annot>
